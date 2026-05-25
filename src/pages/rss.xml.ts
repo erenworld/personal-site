2b4e11ea5b869fe029e734381e430b91c33c1159
Eren Turkoglu <erenxturkoglu@gmail.com>
2026-05-25 14:49:26 +0200

    Fix RSS content rendering

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -5,6 +5,19 @@ import MarkdownIt from "markdown-it";
 import { getBlogPosts } from "../data/utils";
 
 const parser = new MarkdownIt();
+const allowedTags = sanitizeHtml.defaults.allowedTags.concat(["img"]);
+
+const getPostContent = (post: Awaited<ReturnType<typeof getBlogPosts>>[number]) => {
+  if (typeof post.rendered?.html === "string") {
+    return post.rendered.html;
+  }
+
+  if (typeof post.body === "string") {
+    return parser.render(post.body);
+  }
+
+  return "";
+};
 
 export async function GET(context: APIContext) {
   const posts = await getBlogPosts();
@@ -16,9 +29,7 @@ export async function GET(context: APIContext) {
       title: post.data.title,
       pubDate: post.data.date,
       link: `/${post.id}/`,
-      content: sanitizeHtml(parser.render(post.body!), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
-      }),
+      content: sanitizeHtml(getPostContent(post), { allowedTags }),
     })),
   });
 }
